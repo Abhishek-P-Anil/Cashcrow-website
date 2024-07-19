268291be7c09661b7bfea1b2d38013095aa4feb4
abhishek-p-anil <abhishekpanilpallarikkattu@gmail.com>
2024-07-19 18:58:58 +0530

    improved performance and SEO

--- sw.js
+++ sw.js
@@ -1,23 +1,29 @@
+const CACHE_NAME = 'cashcrow-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/assets/css/main.css',
+  '/assets/js/main.js',
+  '/assets/img/CC1trim.png',
+  '/assets/img/CC1.png',
+  // Add other assets here
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('cashcrow-cache').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/assets/css/main.css',
-        '/assets/js/main.js',
-        '/assets/img/CC1trim.png',
-        '/assets/img/CC1.png'
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
 
